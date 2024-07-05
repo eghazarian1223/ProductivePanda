8a8c9485fc59fb2af1ecc9e7c581bfe913b18483
eghazarian1223 <ghaza034@umn.edu>
2024-07-05 18:49:24 -0400

    uncomment config.py

--- server/config/config.py
+++ server/config/config.py
@@ -1,12 +1,15 @@
-
 import os
 from google.cloud import language_v1
 from google.oauth2 import service_account
-# Make sure to put whatever is meant to be in google_places_config.py into this file, and then delete that one
-
 
 def get_nlp_client():
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "server/config/productivepandacredentials.json" 
-    return language_v1.LanguageServiceClient
+    # Load credentials from the environment variable
+    credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+    if credentials_path:
+        credentials = service_account.Credentials.from_service_account_file(credentials_path)
+        return language_v1.LanguageServiceClient(credentials=credentials)
+    else:
+        raise EnvironmentError("GOOGLE_APPLICATION_CREDENTIALS environment variable not set")
 
 
