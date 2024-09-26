aa0f79dccad8599fee5f6d6162cad347ab6fa95a
eghazarian1223 <ghaza034@umn.edu>
2024-09-26 10:35:15 -0400

    Add config.py file

--- server/config/config.py
+++ server/config/config.py
@@ -4,13 +4,19 @@ from google.oauth2 import service_account
 
 def get_nlp_client():
     # Load credentials from the environment variable
-    credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+    try:
 
-    if credentials_path:
-        credentials = service_account.Credentials.from_service_account_file(credentials_path)
-        return language_v1.LanguageServiceClient(credentials=credentials)
-    else:
-        raise EnvironmentError("GOOGLE_APPLICATION_CREDENTIALS environment variable not set")
+        credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
+
+        if credentials_path:
+            credentials = service_account.Credentials.from_service_account_file(credentials_path)
+            print("Google NLP client initialized successfully with provided credentials.")
+            return language_v1.LanguageServiceClient(credentials=credentials)
+        else:
+            raise EnvironmentError("GOOGLE_APPLICATION_CREDENTIALS environment variable not set")
+    except Exception as e:
+        print(f"Error initializing Google NLP client: {e}")
+        raise
 
 def get_firestore_client():
     credentials_path = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
